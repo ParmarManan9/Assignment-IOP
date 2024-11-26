f8fdaa94de14e2057a921c4ebe5e9421408b0cda
ParmarManan9 <u24ai081@aid.svnit.ac.in>
2024-11-27 00:36:11 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,20 @@
+// PRINT FIBONACCI SERIES.(0,1,1,2,3...)
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter a number : ");
+  scanf("%d",&n);
+  printf("The Fibonacci Series : 1 1 ");
+  int a=1;
+  int b=1;
+  int sum=0;
+  for(int i=1 ; i<=n-2 ; i++)
+    {
+      sum=a+b;
+      a=b;
+      b=sum;
+      printf("%d ",sum);
+    }
+  return 0;
+}
